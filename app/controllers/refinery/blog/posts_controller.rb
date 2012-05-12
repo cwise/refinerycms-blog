e7cceeade6d2237047381ee7d5e962560ca23132
Chris Wise <cwise@murmurinformatics.com>
2012-05-12 13:13:02 -0400

    fixed a bug when rendering in a locale other than English with a link

--- app/controllers/refinery/blog/posts_controller.rb
+++ app/controllers/refinery/blog/posts_controller.rb
@@ -22,7 +22,7 @@ module Refinery
       def show
         @comment = Comment.new
 
-        @canonical = url_for(:locale => ::Refinery::I18n.default_frontend_locale) if canonical?
+        @canonical = refinery.url_for(:locale => ::Refinery::I18n.default_frontend_locale) if canonical?
 
         @post.increment!(:access_count, 1)
 
